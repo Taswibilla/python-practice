5539f1bb1666eca33194922335d9305c7e05b3e6
taswi <taswibilla5@gmail.com>
2026-03-20 23:08:50 +0530

    Added multithreading and multiprocessing code

---
+++ multithreadprocess.py
@@ -0,0 +1,21 @@
+#multithreading
+import threading
+
+def task():
+    print("Thread running")
+
+t1 = threading.Thread(target=task)
+t1.start()
+t1.join()
+print("Thread finished")
+#multiprocessing
+from multiprocessing import Process
+
+def task():
+    print("Process running")
+
+if __name__ == "__main__":
+    p1 = Process(target=task)
+    p1.start()
+    p1.join()
+    print("Process finished")
