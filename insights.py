5529d6322f6ddb11ecfd974ee075c8a2c53ec895
taswi <taswibilla5@gmail.com>
2026-04-05 11:16:32 +0530

    insights of assignment

---
+++ insights.py
@@ -0,0 +1,10 @@
+#insights
+#1.Missing values were cleaned, so the data became complete and usable.
+
+#2. Filtering helped to identify students with higher marks.
+
+#3. Grouping showed average performance by category (like salary).
+
+#4. New columns like Total and Result made analysis easier.
+
+#5. Graphs helped to understand data distribution and performance clearly.
