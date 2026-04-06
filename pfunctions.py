df3da2808cfad18898653f020a631c4f3ab9be3b
taswi <taswibilla5@gmail.com>
2026-04-06 11:57:54 +0530

    pandas implementation

--- pfunctions.py
+++ pfunctions.py
@@ -8,6 +8,10 @@ data = {
 }
 df = pd.DataFrame(data)
 print("DataFrame:\n", df)
+# 1.data cleaning
+# Check for missing values
+print("\nMissing values:\n", df.isnull().sum())
+print(df.fillna(0,inplace=True))
 
 # 2. View first rows
 print("\nFirst 2 rows:\n", df.head(2))
@@ -23,7 +27,7 @@ df['Age+10'] = df['Age'] + 10
 print("\nAfter adding column:\n", df)
 
 # 6. Drop column
-df.drop('Age+10', axis=1, inplace=True)
+df = df.drop('Age+10', axis=1)
 print("\nAfter dropping column:\n", df)
 
 # 7. Sort by Age
@@ -32,3 +36,8 @@ print("\nSort by Age descending:\n", df.sort_values('Age', ascending=False))
 # 8. Basic stats
 print("\nMean Age:", df['Age'].mean())
 print("Max Salary:", df['Salary'].max())
+
+print(df.describe())
+# Grouping
+grouped = df.groupby('Name')['Age'].mean()
+print("\nAverage Age by Name:\n", grouped)
